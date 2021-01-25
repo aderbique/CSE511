234a9b4fc5773b0caabe6bd4dbf37be1b00f9a82
Austin Derbique <austin@derbique.org>
2021-01-24 20:08:20 -0700

    add files

---
+++ assignment1/solution.sql
@@ -0,0 +1,8 @@
+i/* Author: Austin Derbique
+    Course: CSE 511 Data Processing
+    Term: Spring 2021
+    Assignment #: 1
+    Assignment Description: Create Movie Recommendation Database 
+*/
+
+/* Create users table */
