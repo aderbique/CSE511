35efd3108ee1d4151f4681570527f6acd57f8786
Austin Derbique <austin@derbique.org>
2021-03-29 23:18:26 -0700

    begin work on part 1

--- assignment4/Interface.py
+++ assignment4/Interface.py
@@ -8,9 +8,12 @@ import os
 import sys
 # Donot close the connection inside this file i.e. do not perform openconnection.close()
 def RangeQuery(ratingsTableName, ratingMinValue, ratingMaxValue, openconnection):
-    pass
-
-
+    cur = openconnection.cursor()
+    if os.path.exists('RangeQueryOut.txt'): os.remove('RangeQueryOut.txt')
+    with open('RangeQueryOut.txt', 'w') as f: f.write('PartitionName, UserID, MovieID, Rating\n')
+    cur.execute("SELECT MAX(partitionnum) + 1 FROM rangeratingsmetadata;")
+    range_len = int(cur.fetchone()[0])
+    
 
 def PointQuery(ratingsTableName, ratingValue, openconnection):
     pass
