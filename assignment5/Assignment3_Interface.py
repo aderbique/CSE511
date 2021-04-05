46c9dfee1efc46ceb6c0ab43872ed7bda5aa22b9
Austin Derbique <austin@derbique.org>
2021-04-04 20:24:13 -0700

    add starter files

---
+++ assignment5/Assignment3_Interface.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python2.7
+#
+# Assignment3 Interface
+#
+
+import psycopg2
+import os
+import sys
+
+# Donot close the connection inside this file i.e. do not perform openconnection.close()
+def ParallelSort (InputTable, SortingColumnName, OutputTable, openconnection):
+    #Implement ParallelSort Here.
+    pass #Remove this once you are done with implementation
+
+def ParallelJoin (InputTable1, InputTable2, Table1JoinColumn, Table2JoinColumn, OutputTable, openconnection):
+    #Implement ParallelJoin Here.
+    pass # Remove this once you are done with implementation
