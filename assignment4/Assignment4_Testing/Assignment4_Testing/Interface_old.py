b3680939909dbdd7c7a2611eaa767a82b0b55299
Austin Derbique <austin@derbique.org>
2021-04-21 12:48:45 -0700

    add old interface

--- assignment4/Assignment4_Testing/Assignment4_Testing/Interface_old.py
+++ assignment4/Assignment4_Testing/Assignment4_Testing/Interface_old.py
@@ -9,81 +9,90 @@ import sys
 # Donot close the connection inside this file i.e. do not perform openconnection.close()
 def RangeQuery(ratingsTableName, ratingMinValue, ratingMaxValue, openconnection):
     fw = open("./RangeQueryOut.txt", 'w')
-    # Query Range Tables
+    #if os.path.exists('RangeQueryOut.txt'): os.remove('RangeQueryOut.txt')
+    #with open('RangeQueryOut.txt', 'w') as f:
+    #    f.write('PartitionName, UserID, MovieID, Rating\n')
     cur = openconnection.cursor()
-    if os.path.exists('RangeQueryOut.txt'): os.remove('RangeQueryOut.txt')
-    with open('RangeQueryOut.txt', 'w') as f:
-        f.write('PartitionName, UserID, MovieID, Rating\n')
-    cur.execute("SELECT MAX(partitionnum) + 1 FROM rangeratingsmetadata;")
-    range_parts = int(cur.fetchone()[0])
+    # Query Range Tables
+    cur.execute("SELECT table_name FROM information_schema.tables WHERE table_name LIKE 'roundrobinratingspart%'")
+    range_partition = []
+    for item in cur:
+        range_partition.append(item[0])
 
-    for part in range(range_parts):
-        rq =  "SELECT 'RangeRatingsPart" + str(part) + "' AS PartitionName, UserID, MovieID, Rating " \
-              "FROM rangeratingspart" + str(part) + " " +\
-              "WHERE rating <= " + str(ratingMaxValue) + " AND rating >= " + str(ratingMinValue)
-
-        cur.execute(rq)
-        for result in cur:
-            fw.write("{0},{1},{2},{3}\n".format(part, result[0], result[1], result[2]))
-        #results = cur.fetchall()
-        #writeToFile('RangeQueryOut.txt', results) 
-
-    # Query Round Robin
-    cur.execute("SELECT partitionnum FROM roundrobinratingsmetadata;")
-    rr = int(cur.fetchone()[0])
-    for part in range(rr):
-        rq = "SELECT 'RoundRobinRatingsPart" + str(part) + "' AS PartitionName, UserID, MovieID, Rating " \
-           "FROM roundrobinratingspart" + str(part) + " " + \
-           "WHERE rating <= " + str(ratingMaxValue) + " AND rating >= " + str(ratingMinValue)
-        cur.execute(rq)
-        for result in cur:
-            fw.write("{0},{1},{2},{3}\n".format(part, result[0], result[1], result[2]))
+    for tablename in range_partition:
+        cur.execute("SELECT * FROM {} WHERE rating >= {} and rating <= {}".format(tablename, ratingMinValue, ratingMaxValue))
         #results = cur.fetchall()
         #writeToFile('RangeQueryOut.txt', results)
-        
-    openconnection.commit()
+        for calc in cur:
+            fw.write("{},{},{},{}\n".format(tablename, calc[0], calc[1], calc[2]))
+
+    cur.execute("SELECT * FROM rangeratingsmetadata")
+    range_partition = []
+    for item in cur:
+        range_partition.append(item)
+    # Query Round Robin
+    for item in range_partition:
+        if float(item[1]) > ratingMaxValue or float(item[2]) < ratingMinValue:
+            continue
+        elif float(item[1]) >= ratingMinValue and float(item[2]) <= ratingMinValue:
+            cur.execute("SELECT * FROM rangeratingspart{}".format(item[0]))
+            #results = cur.fetchall()
+            #writeToFile('RangeQueryOut.txt', results)
+            for calc in cur:
+                fw.write("rangeratingspart{},{},{},{}\n".format(item[0], calc[0], calc[1], calc[2]))
+        else:
+            cur.execute("SELECT * FROM rangeratingspart{} WHERE rating >= {} and rating <= {}".format(item[0], ratingMinValue,ratingMaxValue))
+            #results = cur.fetchall()
+            #writeToFile('RangeQueryOut.txt', results)
+            for calc in cur:
+                fw.write("rangeratingspart{},{},{},{}\n".format(item[0], calc[0], calc[1], calc[2]))
+
     fw.close()
-    cur.close()     
+    cur.close()
 
 def PointQuery(ratingsTableName, ratingValue, openconnection):
-    fw = open("./PointQueryOut.txt", 'w')
     # Query Range Tables
+    fw = open("./PointQueryOut.txt", 'w')
+    #if os.path.exists('PointQueryOut.txt'): os.remove('PointQueryOut.txt')
+    #with open('PointQueryOut.txt', 'w') as f:
+    #    f.write('PartitionName, UserID, MovieID, Rating\n')
     cur = openconnection.cursor()
-    if os.path.exists('PointQueryOut.txt'): os.remove('PointQueryOut.txt')
-    with open('PointQueryOut.txt', 'w') as f:
-        f.write('PartitionName, UserID, MovieID, Rating\n')
-    cur.execute("SELECT MAX(partitionnum) + 1 FROM rangeratingsmetadata;")
-    point_parts = int(cur.fetchone()[0])
 
-    for part in range(point_parts):
-        pq = "SELECT 'RangeRatingsPart" + str(part) + "' AS PartitionName, UserID, MovieID, Rating " \
-             "FROM rangeratingspart" + str(part) + " " + \
-             "WHERE rating = " + str(ratingValue)    
-        cur.execute(pq)
-        for result in cur:
-            fw.write("{0},{1},{2},{3}\n".format(part, result[0], result[1], result[2]))
-        #results = cur.fetchall()
-        #writeToFile('PointQueryOut.txt', results) 
+    cur.execute("SELECT table_name FROM information_schema.tables WHERE table_name LIKE 'roundrobinratingspart%'")
+    range_partition = []
+    for item in cur:
+        range_partition.append(item[0])
 
-    # Query Round Robin
-    cur.execute("SELECT partitionnum FROM roundrobinratingsmetadata;")
-    rr = int(cur.fetchone()[0])
-    for part in range(rr):
-        pq = "SELECT 'RoundRobinRatingsPart" + str(part) + "' AS PartitionName, UserID, MovieID, Rating " \
-             "FROM roundrobinratingspart" + str(part) + " " + \
-             "WHERE rating = " + str(ratingValue)
-        cur.execute(pq)
-        for result in cur:
-            fw.write("{0},{1},{2},{3}\n".format(part, result[0], result[1], result[2]))
+    for tablename in range_partition:
+        cur.execute("SELECT * FROM {} WHERE rating = {}".format(tablename, ratingValue))
         #results = cur.fetchall()
         #writeToFile('PointQueryOut.txt', results)
-    openconnection.commit()
-    fw.close()
-    cur.close()    
+        for calc in cur:
+            fw.write("{},{},{},{}\n".format(tablename, calc[0], calc[1], calc[2]))
 
-def writeToFile(filename, rows):
+    cur.execute("SELECT * FROM rangeratingsmetadata")
+    range_partition = []
+    for item in cur:
+        range_partition.append(item)
+
+    # Query Round Robin
+    for item in range_partition:
+        if float(item[1]) > ratingValue or float(item[2]) < ratingValue:
+            continue
+        else:
+            cur.execute("SELECT * FROM rangeratingspart{} WHERE rating = {}".format(item[0], ratingValue))
+
+            #results = cur.fetchall()
+            #writeToFile('PointQueryOut.txt', results)
+            for calc in cur:
+                fw.write("rangeratingspart{},{},{},{}\n".format(item[0], calc[0], calc[1], calc[2]))
+
+    fw.close()
+    cur.close()
+
+def writeToFile(filename, items):
     f = open(filename, 'a')
-    for line in rows:
+    for line in items:
         f.write(','.join(str(s) for s in line))
         f.write('\n')
     f.close()
